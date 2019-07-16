cc65cf7c9e594f9d0539ddac3a112ee82046d2f5
guischpor <guischpor1@live.com>
2019-07-16 11:00:07 -0300

    update

--- index.php
+++ index.php
@@ -2,11 +2,42 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
+    <!--Import Google Icon Font-->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--CSS MATERIALIZE-->
+    <link rel="stylesheet" href="materialize/css/materialize.min.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Cadastro de Pessoas PHP</title>
 </head>
 <body>
+
+    <!-- Nav Menu -->
+    <nav class="blue-grey">
+        <div class="nav-wrapper container">
+            <div class="brand-logo light">Sistema de Cadastro</div>
+            <ul class="right">
+                <li><a href=""><i class="material-icons left">account_circle</i>Cadastro</a></li>
+                <li><a href=""><i class="material-icons left">search</i>Consultas</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <!-- Formulário de Cadastro -->
     
+
+        
+
+
+    <!-- Arquivos Importados JQUERY -->
+    <script type="text/javascript" src="materialize/js/jquery-3.4.1.min.js"></script>
+    <script type="text/javascript" src="materialize/js/materialize.min.js"></script>
+
+    <!-- Inicialização JQUERY -->
+    <script type="text/javascript">
+        $(document).ready(function() {
+
+        });
+    </script>
 </body>
 </html>
