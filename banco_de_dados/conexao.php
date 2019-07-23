b1ea22e24dcf2374f8bc5211f85bd68625f2f11e
guischpor <guischpor1@live.com>
2019-07-23 09:26:11 -0300

    connexio php update

--- banco_de_dados/conexao.php
+++ banco_de_dados/conexao.php
@@ -1,6 +1,11 @@
 <?php
 //conexão com banco de dados
 $utf8 = header( "Content-Type: text/html; charset=utf-8" );
-//link de conexao com o localhost, nome de usuario, senha, e nome do banco de dados
-$link = new mysqli( 'sql200.main-hosting.eu', 'u285936505_sys', 'wallaBEway@4545', 'u285936505_sys' );
+
+$localhost = 'sql200.main-hosting.eu';
+$user = 'u285936505_sys';
+$password = 'wallaBEway@4545';
+$db_name = 'u285936505_sys';
+
+$link = new mysqli( $localhost, $user, $password, $db_name );
 $link -> set_charset( 'utf8' );
